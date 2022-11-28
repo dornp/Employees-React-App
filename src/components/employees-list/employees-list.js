f1f869533fa02d89947b494ef1e80c6a2b5155fa
Nadezhda <oleneva96@gmail.com>
2022-11-28 16:41:34 +0200

    created user removal

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,13 +2,15 @@ import EmployeesListItem from '../emloyees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onDelete}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
-        console.log(id);
         return (
-            <EmployeesListItem key={id} {...itemProps}/>
+            <EmployeesListItem 
+            key={id} 
+            {...itemProps}
+            onDelete={() => onDelete(id)}/>
         )
     })
 
